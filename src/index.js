b2442c4fe3239b263a75bd668e2d21f5b858b825
abdel <ammegahe@amazon.com>
2023-04-16 18:27:49 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+const form = document.getElementById("#create-task-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.target.new_task_description.value);
 });
+})
